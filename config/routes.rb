c0abb64592c2b2d19aa516d992773935477e85e4
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-17 12:57:48 +0100

    task no style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # create 1/form get 2/ post form
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # show all
+  get 'tasks', to: 'tasks#index'
+  # show one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # updating 1/get edit 2/patch the form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
